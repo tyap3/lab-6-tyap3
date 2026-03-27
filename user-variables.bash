339c5371118f8eedc30c48e6ee0d9b40806e3d04
tyap3 <tyap3@myseneca.ca>
2026-03-27 18:59:03 +0000

    Created all lab 6 files

---
+++ user-variables.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Twicee: Twicee Yap; tyap3@myseneca.ca
+# Date: March 27, 2026
+# Purpose: To demonstrate user variables.
+# Usage: ./user-variables.bash
+
+read -p "Enter your Full Name: " name
+read -p "Enter your age (in years): " age
+
+echo "Hello $name - You are $age years old"
